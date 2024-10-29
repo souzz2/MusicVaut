a4baa6faac35cd19a5bf97efd655a731e9bcf6a7
souzz2 <108484845+souzz2@users.noreply.github.com>
2024-10-29 19:05:28 -0300

    Update functionsArtists.ts

--- src/artists/functionsArtists.ts
+++ src/artists/functionsArtists.ts
@@ -1,8 +1,8 @@
-import connection from "../connections";
+import connection from "../connection";
 import { Artist } from "./typeArtist";
 
 const searchArtistsByName = async (name: string): Promise<Artist[]> => {
-  return await connection("artists").where("nameartist", "like", `%${name}%`);
+  return await connection("artists").select("nameartist").where("nameartist", "like", `%${name}%`).orderBy("nameartist", "asc").limit(5);
 };
 
 module.exports = {
