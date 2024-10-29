58842cbb74aadb3ef40a3a1873440cae3b8d2265
souzz2 <108484845+souzz2@users.noreply.github.com>
2024-10-29 19:05:32 -0300

    Create connection.ts

---
+++ src/connection.ts
@@ -0,0 +1,17 @@
+import knex, { Knex } from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = knex({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    port: 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
+    multipleStatements: true,
+  },
+});
+export default connection;  
